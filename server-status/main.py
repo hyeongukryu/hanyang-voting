e07acc19154a8b440c6a369033cc602ce3463ee0
류형욱 <mintrupt@mintrupt.com>
2013-08-20 18:27:42 +0900

    템플릿 적용

--- server-status/main.py
+++ server-status/main.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+# -*- coding: utf-8 -*-
+
+import os, datetime, base64
+import webapp2, jinja2
+
+# Jinja2 템플릿
+template_dir = os.path.join(os.path.dirname(__file__), 'template')
+
+jinja2_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+def render_template(template_name):
+    return jinja2_env.get_template(template_name + '.html').render(nav=template_name)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(render_template('main'))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
