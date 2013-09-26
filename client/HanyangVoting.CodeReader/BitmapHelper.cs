8078e665f170e0b996e77255d706da1f6e6515e6
류형욱 <mintrupt@mintrupt.com>
2013-09-26 17:14:00 +0900

    CodeReader 프로젝트 추가

---
+++ client/HanyangVoting.CodeReader/BitmapHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace HanyangVoting.CodeReader
+{
+    public static class BitmapHelper
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            if (bitmap == null)
+            {
+                return null;
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Bmp);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var bi = new BitmapImage();
+                bi.BeginInit();
+                bi.StreamSource = stream;
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.EndInit();
+
+                return bi;
+            }
+        }
+
+        public static Rectangle GetRectangle(this Bitmap bitmap)
+        {
+            return new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+        }
+
+        public static Bitmap CloneBitmap(this Bitmap bitmap)
+        {
+            return bitmap.Clone(GetRectangle(bitmap), PixelFormat.DontCare);
+        }
+    }
+}
